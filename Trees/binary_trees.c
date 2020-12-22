732023a6d44b20413e1e76b6681dd9985afdda1f
Raghav <63911252+raghavpatpatia@users.noreply.github.com>
2020-12-22 11:49:32 +0530

    Update binary_trees.c

--- Trees/binary_trees.c
+++ Trees/binary_trees.c
@@ -1,26 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
-    int data;
-    struct node* left;
-    struct node* right;
+struct node
+{
+     int data;
+     struct node* left;
+     struct node* right;
 };
 
-struct node* newNode(int data){
-    struct node* node = (struct node*)malloc(sizeof(struct node));
-    node -> data = data;
-    node -> left = NULL;
-    node -> right = NULL;
-    return(node);
+struct node* newNode(int data)
+{
+     struct node* node = (struct node*)malloc(sizeof(struct node));
+     node->data = data;
+     node->left = NULL;
+     node->right = NULL;
+     return(node);
 }
 
-int main(){
-    struct node *root = newNode(1);
-    root -> left = newNode(2);
-    root -> right = newNode(3);
+void printPreorder(struct node* node)
+{
+     if (node == NULL)
+          return;
 
-    root -> left -> left = newNode(4);
+     printf("%d ", node->data);
 
-    return 0;
+     printPreorder(node->left);
+
+     printPreorder(node->right);
+}
+
+int main()
+{
+     struct node *root  = newNode(1);
+     root->left         = newNode(2);
+     root->right        = newNode(3);
+     root->left->left   = newNode(4);
+     root->left->right  = newNode(5);
+
+     printf("Preorder traversal of binary tree: \n");
+     printPreorder(root);
+
+     getchar();
+     return 0;
 }
